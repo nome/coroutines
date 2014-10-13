c995e849e3937bf87fdaa47b42d8f983b4320a0b
Knut Franke <Knut.Franke@gmx.de>
2014-10-13 19:59:03 +0200

    Clean up and extend parse_apache example
    Add code comments and implement follow mode (like tail -f). Also fixes
    scaling of request_rate.

--- examples/parse_apache.rb
+++ examples/parse_apache.rb
@@ -5,6 +5,7 @@ require 'date'
 # http://www.dabeaz.com/generators
 
 class String
+	# parse Apache timestamps
 	def to_datetime
 		DateTime.parse(sub(":", "T"))
 	end
@@ -13,6 +14,10 @@ end
 COLNAMES = %w{host referrer user datetime    method request proto status bytes}
 COLTYPES = %w{to_s to_s     to_s to_datetime to_s   to_s    to_s  to_i   to_i }.map(&:to_sym)
 
+# Reads log lines in the following format:
+# host referrer user [timestamp] "GET|POST request protocol" status bytes ...
+# For each parsable line, yields a Hash with the keys given by COLNAMES and the
+# values parsed according to COLTYPES.
 def parse
 	loop do
 		if await =~ /(\S+) (\S+) (\S+) \[(.*?)\] "(\S+) (\S+) (\S+)" (\S+) (\S+)/
@@ -24,18 +29,13 @@ def parse
 	end
 end
 
+# Reads log entry Hashes; for each log entry with status 404, yields 
 def find_404
 	loop do
 		r = await
 		if r["status"] == 404
 			yield %w{status datetime request}.map{|col| r[col].to_s}.join(" ")
 		end
-	end
-end
-
-def dump(label)
-	loop do
-		puts "#{label}: #{await}"
 	end
 end
 
@@ -47,6 +47,7 @@ def bytes_transferred
 	end
 end
 
+DAY = 24 * 60 * 60
 def request_rate
 	last = await["datetime"]
 	loop do
@@ -55,12 +56,30 @@ def request_rate
 			this = await["datetime"]
 			n += 1
 		end while this == last
-		yield n.to_f / (this - last).to_f
+		yield n.to_f / (this - last).to_f / DAY
 		last = this
 	end
 end
 
-open("/var/log/apache2/access.log").
+def dump(label)
+	loop do
+		puts "#{label}: #{await}"
+	end
+end
+
+def follow(path)
+	file = open(path)
+	loop do
+		begin
+			yield file.readline
+		rescue EOFError
+			sleep 0.1
+		end
+	end
+	file.close
+end
+
+enum_for(:follow, "/var/log/apache2/access.log").
 	out_connect(trans_for :parse).
 	out_connect(Multicast.new(
 		trans_for(:find_404).         out_connect(consum_for(:dump, "404        ")),
